49d5ac1b764c0c99cfc483d18eed994bd126a169
Qian Cai <quic_qiancai@quicinc.com>
2022-03-23 17:02:56 -0400

    src: Add CPPC tests
    This requires "acpi_cppc" driver and "schedutils" governor. It will
    assume the latest online CPU running at the lowest frequency, and then
    force it to run at the highest frequency. Finally, it should scale down
    once the workload is gone. We will check performance counters to confirm
    them as well.
    
    Signed-off-by: Qian Cai <quic_qiancai@quicinc.com>

--- src/data.py
+++ src/data.py
@@ -3,11 +3,21 @@
 # Copyright (c) 2022, Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import typing
+
 import src.meta as meta
+import src.cppc as cppc
 
 
-def num2case(num: int) -> meta.TestCase:
-    # FIXME
+def num2case(num: int) -> typing.Optional[meta.TestCase]:
     test_case = meta.TestCase()
+    tc_map = {
+        1: (cppc.setup_cppc, cppc.run_cppc, test_case.cleanup, 30, 'scale CPU up and down')
+    }
+
+    if num in tc_map:
+        test_case.setup, test_case.run, test_case.cleanup, test_case.timeout, test_case.name = tc_map[num]
+    else:
+        return None
 
     return test_case
